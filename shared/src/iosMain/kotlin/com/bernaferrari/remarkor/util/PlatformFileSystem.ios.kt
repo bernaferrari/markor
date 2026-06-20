fa528a2732d71908209bd33195a431fecf2495c4
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-06-20 01:24:46 -0300

    Fix CI failures: okio SYSTEM import and Vercel CLI version
    - Use FileSystem.SYSTEM on Android/iOS (okio.SYSTEM unresolved on CI)
    - Upgrade Vercel CLI to latest (API requires >= 47.2.2)
    - Skip lint artifact copy when no lint-results files exist

--- shared/src/iosMain/kotlin/com/bernaferrari/remarkor/util/PlatformFileSystem.ios.kt
+++ shared/src/iosMain/kotlin/com/bernaferrari/remarkor/util/PlatformFileSystem.ios.kt
@@ -1,6 +1,5 @@
 package com.bernaferrari.remarkor.util
 
 import okio.FileSystem
-import okio.SYSTEM
 
-actual val platformFileSystem: FileSystem = SYSTEM
+actual val platformFileSystem: FileSystem = FileSystem.SYSTEM
